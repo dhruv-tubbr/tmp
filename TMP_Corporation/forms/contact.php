99a79c35917e37159070006dc2c3ccedeb32943e
dhruv-tubbr <dhruv@worldnetlabs.com>
2021-09-12 18:29:19 +0530

    Major Updates

--- TMP_Corporation/forms/contact.php
+++ TMP_Corporation/forms/contact.php
@@ -1,13 +1,6 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'vdhruvyt@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -16,26 +9,69 @@
   }
 
   $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  $contact->ajax = false;
+  
   
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $contact->subject = "New Inquiry - ".$_POST['name']." | TMP Global";
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  if(!empty($_POST['name'])){
+    $name= $_POST['name'];
+    $contact->add_message( $name, 'Name');
+  }
+
+  if(!empty($_POST['email'])){
+    $sender_email= $_POST['email'];
+    $contact->add_message( $sender_email, 'Email');
+  }
+
+  if(!empty($_POST['phone_number'])){
+    $phone_number= $_POST['phone_number'];
+    $contact->add_message( $phone_number, 'Phone Number');
+  }
+
+  if(!empty($_POST['company'])){
+    $company_name= $_POST['company'];
+    $contact->add_message( $phone_number, 'Company Name');
+  }
+
+  if(!empty($_POST['message'])){
+    $sender_message= $_POST['message'];
+    $contact->add_message( $sender_message, 'Message',10);
+  }
+   
+  
+
+  if(!empty($_POST['japan'])){
+    $japan = $_POST['japan'];
+    $countj = count($japan);
+    $contact->add_message( $countj, 'Enter Japan Market');
+    for($i=0;$i<$countj;$i++){
+      $contact->add_message( $japan[$i], "• ");
+    }
+  }
+
+  if(!empty($_POST['dx'])){
+    $dx = $_POST['dx'];
+    $countd = count($dx);
+    $contact->add_message( $countd, 'Become Future Ready');
+    for($j=0;$j<$countd;$j++){
+      $contact->add_message( $dx[$j], "• ");
+    }
+  }
+
+  if(!empty($_POST['sector'])){
+    $sector = $_POST['sector'];
+    $counts = count($sector);
+    $contact->add_message( $counts, 'Sectors');
+    for($s=0;$s<$counts;$s++){
+      $contact->add_message( $sector[$s], "• ");
+    }
+  }
 
   echo $contact->send();
 ?>
--- TMP_Corporation/jp/forms/contact.php
+++ TMP_Corporation/jp/forms/contact.php
@@ -11,20 +11,67 @@
   $contact = new PHP_Email_Form;
   $contact->ajax = false;
   
+  
   $contact->to = $receiving_email_address;
-  $contact->from_name = "dhruv";
-  $contact->from_email = "vdhruvyt@gmail.com";
-  $contact->subject = "dhruv mail";
-  // $contact->to = $receiving_email_address;
-  // $contact->from_name = $_POST['name'];
-  // $contact->from_email = $_POST['email'];
-  // $contact->subject = $_POST['subject'];
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = "New Inquiry - ".$_POST['name']." | TMP Japan";
 
 
-  $contact->add_message( "Dhruv", 'From');
-  // $contact->add_message( $_POST['name'], 'From');
-  // $contact->add_message( $_POST['email'], 'Email');
-  // $contact->add_message( $_POST['message'], 'Message', 10);
+
+  if(!empty($_POST['name'])){
+    $name= $_POST['name'];
+    $contact->add_message( $name, 'Name');
+  }
+
+  if(!empty($_POST['email'])){
+    $sender_email= $_POST['email'];
+    $contact->add_message( $sender_email, 'Email');
+  }
+
+  if(!empty($_POST['phone_number'])){
+    $phone_number= $_POST['phone_number'];
+    $contact->add_message( $phone_number, 'Phone Number');
+  }
+
+  if(!empty($_POST['company'])){
+    $company_name= $_POST['company'];
+    $contact->add_message( $phone_number, 'Company Name');
+  }
+
+  if(!empty($_POST['message'])){
+    $sender_message= $_POST['message'];
+    $contact->add_message( $sender_message, 'Message',10);
+  }
+   
+  
+
+  if(!empty($_POST['product'])){
+    $product = $_POST['product'];
+    $countp = count($product);
+    $contact->add_message( $countp, 'TMP Products');
+    for($i=0;$i<$countp;$i++){
+      $contact->add_message( $product[$i], "• ");
+    }
+  }
+
+  if(!empty($_POST['dx'])){
+    $dx = $_POST['dx'];
+    $countd = count($dx);
+    $contact->add_message( $countd, 'Become Future Ready');
+    for($j=0;$j<$countd;$j++){
+      $contact->add_message( $dx[$j], "• ");
+    }
+  }
+
+  if(!empty($_POST['sector'])){
+    $sector = $_POST['sector'];
+    $counts = count($sector);
+    $contact->add_message( $counts, 'Sectors');
+    for($s=0;$s<$counts;$s++){
+      $contact->add_message( $sector[$s], "• ");
+    }
+  }
 
   echo $contact->send();
 ?>
